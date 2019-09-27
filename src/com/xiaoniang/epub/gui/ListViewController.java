decc69f2504579678236c9e1f10190490e5dd9a8
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2019-09-27 16:59:46 +0300

    changed IDE

--- src/com/xiaoniang/epub/gui/ListViewController.java
+++ src/com/xiaoniang/epub/gui/ListViewController.java
@@ -8,14 +8,17 @@ import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.util.Callback;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+
+import com.xiaoniang.epub.resources.Links;
 
 public class ListViewController
 {
     @FXML
     private ListView<String> listView;
-    private Set<String> stringSet = new HashSet();
+    private ArrayList<String> stringSet = new ArrayList<String>();
     ObservableList<String> observableList = FXCollections.observableArrayList();
     private Scene scene;
 
@@ -36,10 +39,9 @@ public class ListViewController
 
     public void setListView()
     {
-    	stringSet.add("String 1");
-        stringSet.add("String 2");
-        stringSet.add("String 3");
-        stringSet.add("String 4");
+    	for (String link : Links.links().keySet()) {
+			stringSet.add(link);
+		}
         observableList.setAll(stringSet);
         listView.setItems(observableList);
         listView.setCellFactory(new Callback<ListView<String>, javafx.scene.control.ListCell<String>>()
