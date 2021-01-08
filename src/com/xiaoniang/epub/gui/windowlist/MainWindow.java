ba2040a6458f7ce811c277aa4927d3447ba32182
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-01-08 23:31:49 +0300

    update

--- src/com/xiaoniang/epub/gui/windowlist/MainWindow.java
+++ src/com/xiaoniang/epub/gui/windowlist/MainWindow.java
@@ -10,26 +10,26 @@ import java.util.ArrayList;
 
 public class MainWindow extends Application {
 
-	@Override
-	public void start(Stage stage) {
-		initUI(stage);
-	}
+    @Override
+    public void start(Stage stage) {
+        initUI(stage);
+    }
 
-	private void initUI(Stage stage) {
+    private void initUI(Stage stage) {
 
-		ArrayList<String> strings = new ArrayList<String>();
-		Links.fill();
-		MainWindowController mainWindowController = new MainWindowController();
-		try {
-			mainWindowController.setListView();
-		} catch (IOException e) {
-			e.printStackTrace(Log.stream());
-		}
-		stage.setScene(mainWindowController.getScene());
-		stage.show();
-	}
+        ArrayList<String> strings = new ArrayList<String>();
+        Links.fill();
+        MainWindowController mainWindowController = new MainWindowController();
+        try {
+            mainWindowController.setListView();
+        } catch (IOException e) {
+            e.printStackTrace(Log.stream());
+        }
+        stage.setScene(mainWindowController.getScene());
+        stage.show();
+    }
 
-	public static void create(String[] args) {
-		launch(args);
-	}
+    public static void create(String[] args) {
+        launch(args);
+    }
 }
