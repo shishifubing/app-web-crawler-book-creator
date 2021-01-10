e5f93859061c064f371bbb2d4286aebd018dc16c
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-01-10 23:34:49 +0300

    update

--- src/com/xiaoniang/epub/gui/windowlist/listcell/ListViewCell.java
+++ src/com/xiaoniang/epub/gui/windowlist/listcell/ListViewCell.java
@@ -3,18 +3,14 @@ package com.xiaoniang.epub.gui.windowlist.listcell;
 import com.xiaoniang.epub.api.EpubBook;
 import javafx.scene.control.ListCell;
 
-public class ListViewCell extends ListCell<EpubBook>
-{
+public class ListViewCell extends ListCell<EpubBook> {
     @Override
-    public void updateItem(EpubBook EpubBook, boolean empty)
-    {
-        super.updateItem(EpubBook,empty);
-        if(EpubBook != null)
-        {
+    public void updateItem(EpubBook EpubBook, boolean empty) {
+        super.updateItem(EpubBook, empty);
+        if (EpubBook != null) {
             Data data = new Data();
             data.setInfo(EpubBook);
             setGraphic(data.getBox());
         }
     }
 }
-
