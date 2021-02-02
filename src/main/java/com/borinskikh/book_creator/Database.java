ebb1797b4b2a4f63062e811eff652997ae4b2dd1
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-02-02 23:55:46 +0300

    update

---
+++ src/main/java/com/borinskikh/book_creator/Database.java
@@ -0,0 +1,18 @@
+package com.borinskikh.book_creator;
+
+import java.sql.*;
+
+public class Database {
+    public static void main(String[] args) {
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:src/main/java/com/borinskikh/book_creator/Books.db");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
