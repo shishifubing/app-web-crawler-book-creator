34986e071f99f4917a800b48cd504ebea1c79f9c
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-02-04 00:16:18 +0300

    got the database to work

--- src/main/java/com/borinskikh/book_creator/Database.java
+++ src/main/java/com/borinskikh/book_creator/Database.java
@@ -4,15 +4,25 @@ import java.sql.*;
 
 public class Database {
     public static void main(String[] args) {
-        Connection c = null;
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:src/main/java/com/borinskikh/book_creator/Books.db");
+        ResultSet resultSet = null;
+        String connectionUrl = "jdbc:sqlserver://localhost;user=sa;password=Pa$$word;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+                Statement statement = connection.createStatement();) {
+            // Create and execute a SELECT SQL statement.
+            String query = "SELECT id, title, author FROM books";
+            resultSet = statement.executeQuery(query);
+
+            // Print results from select statement
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getString(1) + " / " + resultSet.getString(2) + " / " + resultSet.getString(3));
+            }
+
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
     }
 }
