a00fe62d5d6f6c1122b6c041a123b9e955756f74
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-03-07 15:47:10 +0300

    update

--- src/temp/main.py
+++ src/temp/main.py
@@ -3,6 +3,23 @@ import re
 from bs4 import BeautifulSoup, SoupStrainer
 
 
+def getSitemap():
+    sitemapURL = 'https://www.wuxiaworld.com/sitemap/chapters/1'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+               'Accept': 'text/html'}
+    response = requests.get(sitemapURL, headers=headers)
+    if (response.status_code == 200):
+        cssSelectorChapterText = '#chapter-content'
+        cssSelectorRemoveElements = 'script, style, .chapter-nav'
+        try:
+            chapter = BeautifulSoup(response.content, 'lxml-xml')
+        except IndexError:
+            print('no results')
+        else:
+            with open("./sitemaps/sitemap1.xml", 'w', encoding="utf-8") as openedFile:
+                openedFile.write(chapter.prettify(formatter="html"))
+
+
 def getLinksFromSitemaps():
     with open("./sitemaps/sitemap.xml", 'r', encoding="utf-8") as openedSitemap1, open("./sitemaps/sitemap2.xml", 'r', encoding="utf-8") as openedSitemap2:
         onlyLinks = SoupStrainer('loc')
@@ -21,30 +38,34 @@ def getLinksFromSitemaps():
         return links
 
 
-siteURL = 'https://www.wuxiaworld.com'
-chapterURL = siteURL+'/novel/i-shall-seal-the-heavens/issth-book-1-chapter-1'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
-           'Accept': 'text/html'}
-response = requests.get(chapterURL, headers=headers)
-if (response.status_code == 200):
-    cssSelectorChapterText = '#chapter-content'
-    cssSelectorRemoveElements = 'script, style, .chapter-nav'
-    try:
-        chapter = BeautifulSoup(response.content, 'html.parser')
-        chapterText = chapter.select(cssSelectorChapterText, limit=1)[0]
-    except IndexError:
-        print('no results')
+def getChapter(domainURL, chapterURL):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+               'Accept': 'text/html'}
+    response = requests.get(chapterURL, headers=headers)
+    if (response.status_code == 200):
+        cssSelectorChapterText = '#chapter-content'
+        cssSelectorRemoveElements = 'script, style, .chapter-nav'
+        try:
+            chapter = BeautifulSoup(response.content, 'html.parser')
+            chapterText = chapter.select(cssSelectorChapterText, limit=1)[0]
+        except IndexError:
+            print('no results')
+        else:
+            chapterText['id'] = 'chapter-1'
+            chapterText['class'] = 'chapter-wrapper'
+            for uselessElement in chapterText.select(cssSelectorRemoveElements):
+                uselessElement.decompose()
+            for relativeLink in chapterText(href=re.compile('^/')):
+                relativeLink['href'] = domainURL + relativeLink['href']
+            for relativeLink in chapterText(src=re.compile('^/')):
+                relativeLink['src'] = domainURL+relativeLink['src']
+            chapter.smooth()
+            with open("./chapters/chapter1.html", 'w', encoding="utf-8") as openedFile:
+                openedFile.write(chapterText.prettify(formatter="html"))
     else:
-        chapterText['id'] = 'chapter-1'
-        chapterText['class'] = 'chapter-wrapper'
-        for uselessElement in chapterText.select(cssSelectorRemoveElements):
-            uselessElement.decompose()
-        for relativeLink in chapterText(href=re.compile('^/')):
-            relativeLink['href'] = siteURL + relativeLink['href']
-        for relativeLink in chapterText(src=re.compile('^/')):
-            relativeLink['src'] = siteURL+relativeLink['src']
-        chapter.smooth()
-        with open("./chapters/chapter1.html", 'w', encoding="utf-8") as openedFile:
-            openedFile.write(chapterText.prettify(formatter="html"))
-else:
-    print(response.status_code)
+        print(response.status_code)
+
+
+domainURL = 'https://www.wuxiaworld.com'
+for link in getLinksFromSitemaps():
+    getChapter(domainURL, link)
