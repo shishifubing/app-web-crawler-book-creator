7e37dd3f51ed2577469a9be2bb3cbbcd075244b1
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-03-07 14:21:10 +0300

    update

--- src/server/settings.py
+++ src/server/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'books',
-        'USER': 'borinskikh',
-        'PASSWORD': '065412300',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- src/temp/GetChapter.py
+++ src/temp/GetChapter.py
@@ -1,17 +1,27 @@
-
 import requests
-from bs4 import BeautifulSoup
+import re
+from bs4 import BeautifulSoup, SoupStrainer
 
-
-url = 'https://www.wuxiaworld.com/novel/i-shall-seal-the-heavens/issth-book-1-chapter-1'
-userAgent = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=userAgent)
+siteURL = 'https://www.wuxiaworld.com'
+chapterURL = siteURL+'/novel/i-shall-seal-the-heavens/issth-book-1-chapter-1'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+           'Accept': 'text/html'}
+response = requests.get(chapterURL, headers=headers)
 print(response.status_code)
-chapter = BeautifulSoup(response.content, 'lxml').find(id='chapter-content')
-for script in chapter.findAll('script'):
-    script.decompose()
-chapter.smooth()
-chapterText = chapter.prettify(formatter="html")
-with open("./chapter1.html", 'w', encoding="utf-8") as openedFile:
-    openedFile.write(chapterText.replace(
-        'href="/', 'href=https://www.wuxiaworld.com/'))
+
+chapter = BeautifulSoup(response.content, 'html.parser')
+cssSelector = '#chapter-content'
+try:
+    chapterText = chapter.select(cssSelector, limit=1)[0]
+except IndexError:
+    print('no results')
+else:
+    chapterText['id'] = 'chapter-1'
+    chapterText['class'] = 'chapter-wrapper'
+    for uselessElement in chapterText(['script', 'style']):
+        uselessElement.decompose()
+    for relativeLink in chapterText(href=re.compile('^/')):
+        relativeLink['href'] = siteURL+relativeLink['href']
+    chapter.smooth()
+    with open("./chapter1.html", 'w', encoding="utf-8") as openedFile:
+        openedFile.write(chapterText.prettify(formatter="html"))
