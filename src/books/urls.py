4fb033bf1bcbbba2672697d62766b8f347130262
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-03-08 00:11:25 +0300

    url handler and dynamic pages work

--- src/books/urls.py
+++ src/books/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
+    path('0/', views.allBooks),
     path('<int:bookID>/', views.results),
 ]
--- src/books/views.py
+++ src/books/views.py
@@ -1,10 +1,18 @@
+from .models import Books
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.contrib.flatpages.models import FlatPage
+from django.shortcuts import get_object_or_404
 
 
 def results(request, bookID):
-    print('here')
-    response = f"id of the book {bookID}."
-    return HttpResponse(response)
+    flatPage = get_object_or_404(FlatPage, url='/books/')
+    context = {'flatpage': flatPage, 'books': Books.objects.all()}
+    flatPage.content = '<p>test</p>'
+    return render(request, 'flatpages/default.html', context)
+
+
+def allBooks(request):
+    book = Books.objects.get(id=1)
+    flatPage = FlatPage.objects.get(url='/books/')
+    context = {'flatpage': flatPage}
+    return render(request, flatPage.template_name, context)
